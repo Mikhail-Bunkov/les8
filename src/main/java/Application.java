2f2361d4afe504472b562579ab105098a888d787
Mikhail-Bunkov <bunko04@bk.ru>
2021-01-28 01:18:56 +1000

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import frame.MyFrame;
+
+public class Application {
+    public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame();
+
+    }
+}
---
+++ src/main/java/frame/MyFrame.java
@@ -0,0 +1,12 @@
+package frame;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    public MyFrame(){
+        setTitle("Calculator");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(300, 300, 400, 400);
+        setVisible(true);
+    }
+}
